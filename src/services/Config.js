3ce402b5c5dd299830fce36de7797df0f181ed72
Faiq Allam <faiq.kaboel@gmail.com>
2020-04-30 09:37:27 +0700

    add firebase

--- src/services/Config.js
+++ src/services/Config.js
@@ -1,2 +1,12 @@
 // file config ini bisa berisi variabel-variabel lain yang dibutuhkan untuk proses API
-export const domainPath = 'http://localhost:3001';  // simpan url domain server API pada variabel, sehingga bisa dinamis (diganti)
+export const domainPath = "http://localhost:3001"; // simpan url domain server API pada variabel, sehingga bisa dinamis (diganti)
+export const firebaseConfig = {
+  apiKey: "AIzaSyAh2yyStJYCG55fnBjtLHTPWoad7OfuBGo",
+  authDomain: "re-vivere.firebaseapp.com",
+  databaseURL: "https://re-vivere.firebaseio.com",
+  projectId: "re-vivere",
+  storageBucket: "re-vivere.appspot.com",
+  messagingSenderId: "482731211191",
+  appId: "1:482731211191:web:4aafaacbc9635a26285032",
+  measurementId: "G-G9T764R9HN",
+};
